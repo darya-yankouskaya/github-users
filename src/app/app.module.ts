17a804848d669979ef91e9945c9fb99eb7ddc03f
DaryaYankovskaya <darya.yankovskaya@rockspoon.com>
2023-05-22 00:01:36 +0200

    fix: remove interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { AppComponent } from './app.component';
 import { LayoutModule } from './shared/layout/layout.module';
 import { routerFeatureKey } from './shared/store/router/router.state';
 import { CustomRouterStateSerializer } from './core/utils/router-serializer.util';
-import { GithubTokenInterceptor } from './core/interceptors/github-token.interceptor';
 import { sharedFeatureKey } from './shared/store/shared.state';
 import { sharedReducer } from './shared/store/shared.reducers';
 import { environment } from '../environments/environment';
@@ -52,11 +51,6 @@ import { metaReducers } from './core/utils/meta-reducers.util';
     MatSnackBarModule,
   ],
   providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: GithubTokenInterceptor,
-      multi: true,
-    },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: LoadingSpinnerInterceptor,
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   githubAPI: 'https://api.github.com',
-  githubToken: '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',
   production: true,
 };
